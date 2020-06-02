eb86d5f0f41182621e99f0a4c7c9d68e82aa03f1
Anish Kumar <anish2272002@hotmail.com>
2020-06-02 18:37:04 +0530

    Working project

--- page.js
+++ page.js
@@ -1,1 +1,14 @@
-alert("Hello World!");
+var count=0;
+document.querySelector(".compute").addEventListener('click',
+function(){
+    if(count%2==0){
+        document.querySelector(".compute").style.background ='#ffffff';
+        document.querySelector(".compute").style.color = '#000000';
+        count++
+    }
+    else{
+        document.querySelector(".compute").style.background ='#f84e51';
+        document.querySelector(".compute").style.color = '#ffffff';
+        count++
+    }
+})
